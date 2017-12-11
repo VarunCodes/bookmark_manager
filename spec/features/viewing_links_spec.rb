703314932e54f047c72c5408a6fd14b63c5654aa
James Lizamore <jwlizamore@gmail.com>
2017-12-11 16:53:13 +0000

    Added test for viewing links

--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -2,4 +2,13 @@ feature 'Bookmark links' do
   scenario 'displays list of links' do
     expect(page).to have_content("www.youtube.com")
   end
+  scenario 'I can see existing links on the links page' do
+    Link.create(url: 'www.youtube.com', title: "Youtube")
+    visit '/links'
+    expect(page.status_code).to eq 200
+
+    within 'ul#links' do
+      expect(page).to have_content('Youtube')
+    end
+  end
 end
