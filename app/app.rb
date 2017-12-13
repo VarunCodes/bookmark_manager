18c277e60f7af94afe510821104eab175717a11f
Varun Madhukara <mvarun@hotmail.co.uk>
2017-12-13 16:29:50 +0000

    Filtering by tags test and functionality

--- app/app.rb
+++ app/app.rb
@@ -20,12 +20,18 @@ class Bookmark < Sinatra::Base
   end
 
   post '/links' do
-  link = Link.new(url: params[:url],               # 1. Create a link
-                title: params[:title])
-  tag  = Tag.first_or_create(name: params[:tags])  # 2. Create a tag for the link
-  link.tags << tag                                 # 3. Adding the tag to the link's DataMapper collection.
-  link.save                                        # 4. Saving the link.
-  redirect to('/links')
-end
+    link = Link.new(url: params[:url],               # 1. Create a link
+                  title: params[:title])
+    tag  = Tag.first_or_create(name: params[:tags])  # 2. Create a tag for the link
+    link.tags << tag                                 # 3. Adding the tag to the link's DataMapper collection.
+    link.save                                        # 4. Saving the link.
+    redirect to('/links')
+  end
+
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :links
+  end
 
 end
