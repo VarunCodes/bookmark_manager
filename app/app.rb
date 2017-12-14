6a90dc4ff2c5dd8540dfe67aadc1f3c04766af56
Varun Madhukara <mvarun@hotmail.co.uk>
2017-12-14 18:54:34 +0000

    Adding user accounts

--- app/app.rb
+++ app/app.rb
@@ -5,9 +5,22 @@ require 'sinatra/base'
 require_relative './models/link.rb'
 
 class Bookmark < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'super secret'
 
   get '/' do
     "Hello"
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email],
+                       password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/links')
+  end
+
+  get '/users/new' do
+    erb :'users/new'
   end
 
   get '/links' do
@@ -34,4 +47,9 @@ class Bookmark < Sinatra::Base
     erb :links
   end
 
+  helpers do
+   def current_user
+     @current_user ||= User.get(session[:user_id])
+   end
+  end
 end
